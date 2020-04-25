e332eed98bd71834cad963f564eddb0b5452f644
Kari Burt <kandreahburt@gmail.com>
2020-04-24 20:50:51 -0500

    Updates

---
+++ precache-manifest.adfb249337c6a3e9c53577c6868f2f25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e79ba87e95b8c5e699bf4638c2fac4",
+    "url": "/KariAndreah/where-in-the-world/index.html"
+  },
+  {
+    "revision": "692e8e77cab6e266cd36",
+    "url": "/KariAndreah/where-in-the-world/static/css/main.1f442fd3.chunk.css"
+  },
+  {
+    "revision": "90584ed4b4edf4afd0db",
+    "url": "/KariAndreah/where-in-the-world/static/js/2.87f7cea8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KariAndreah/where-in-the-world/static/js/2.87f7cea8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "692e8e77cab6e266cd36",
+    "url": "/KariAndreah/where-in-the-world/static/js/main.594f4167.chunk.js"
+  },
+  {
+    "revision": "eb494b5a384cfb4d10ae",
+    "url": "/KariAndreah/where-in-the-world/static/js/runtime-main.f255e624.js"
+  }
+]);
